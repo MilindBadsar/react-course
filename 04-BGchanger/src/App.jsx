3404867062f3ab6136e079cb0c2f6ccd2fc58c44
Milind <milindbadsar@gmail.com>
2025-01-30 20:13:13 +0530

    Fixed 05-PasswordGenerator repository issue

--- 04-BGchanger/src/App.jsx
+++ 04-BGchanger/src/App.jsx
@@ -4,11 +4,40 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [color, setColor] = useState("pink");
 
   return (
     <>
-      <h1 className="bg-green-500 text-3xl">A BG changer app with vite.</h1>
+      <div
+        className="w-full h-screen duration-200"
+        style={{ backgroundColor: color }}
+      >
+        <div className="fixed flex flex-wrap justify-center bottom-1/2 inset-x-0 px-2">
+          <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+            <button
+              onClick={() => setColor("red")}
+              className="outline-none px-4 py-1 rounded-full shadow-lg text-white"
+              style={{ backgroundColor: "red" }}
+            >
+              Red
+            </button>
+            <button
+              onClick={() => setColor("green")}
+              className="outline-none px-4 py-1 rounded-full shadow-lg text-white"
+              style={{ backgroundColor: "green" }}
+            >
+              Green
+            </button>
+            <button
+              onClick={() => setColor("blue")}
+              className="outline-none px-4 py-1 rounded-full shadow-lg text-white"
+              style={{ backgroundColor: "blue" }}
+            >
+              Blue
+            </button>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
